9c10ec8204dbe23d18eaa23e5618dbadc6a6c232
frankiewei <frankiewei23@gmail.com>
2019-11-30 22:43:21 +1100

    update setting

--- MovieBlog/settings.py
+++ MovieBlog/settings.py
@@ -126,16 +126,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-# deploy model
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# deploy model Heroku
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # debug model
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # media file
 MEDIA_URL = '/media/'
