7529ddd4cb6a45b74a213902286cbbc68a6afe4f
frankiewei <frankiewei23@gmail.com>
2019-11-30 21:55:21 +1100

    update models

--- my_profile/models.py
+++ my_profile/models.py
@@ -6,8 +6,8 @@ from django.urls import reverse
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     date_of_birth = models.DateField(blank=True, null=True)
-    # avatar = models.ImageField(upload_to='users/%Y/%m/%d', blank=True)
-    avatar = models.CharField(max_length=800, null=True)
+    avatar = models.ImageField(upload_to='users/%Y/%m/%d', blank=True, default="")
+    # avatar = models.CharField(max_length=800, null=True)
 
     def __str__(self):
         return 'Profile for user {}'.format(self.user_id)
