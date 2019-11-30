0849a977df30700c72bb82147c921a56d3a003a8
frankiewei <frankiewei23@gmail.com>
2019-11-30 15:00:31 +1100

    update reqirement.txt

--- MovieBlog/settings.py
+++ MovieBlog/settings.py
@@ -129,10 +129,10 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # media file
 MEDIA_URL = '/media/'
