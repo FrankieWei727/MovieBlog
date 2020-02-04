86475af92d0a1d9dd38218d596faddec2254830b
frankiewei <frankiewei23@gmail.com>
2020-02-04 18:38:53 +1100

    deploy setting

--- MovieBlog/settings.py
+++ MovieBlog/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=8&e5@9(1-*t6rtapt(f=kwm@vf!bp7pj7!*h*397tgfd(_j*j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['mlinked.herokuapp.com', '127.0.0.1', 'localhost']
 
