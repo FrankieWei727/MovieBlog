71d1ad20e229343f88a760ee22bee582b226f727
frankiewei <frankiewei23@gmail.com>
2020-04-03 14:51:13 +1100

    fix bugs

--- movie/models.py
+++ movie/models.py
@@ -28,9 +28,6 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
-    def get_absolute_url(self):
-        return reverse('movies:movie_list_by_category', args=[self.slug])
-
 
 """
     Movie model
